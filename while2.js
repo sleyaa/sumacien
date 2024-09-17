3973ac5ce532c60b157b83fdf46535e88b941241
sleyaa <zarekparchmont14@gmail.com>
2024-09-17 17:12:43 -0600

    Add files via upload

---
+++ while2.js
@@ -0,0 +1,9 @@
+//declarar variables 
+let suma = 0;
+let i = 1;
+
+while (i <= 100){
+    suma = suma + i;
+    i++;
+}
+document.write("El resultado de la suma de los numeros del 1 al 100 es: ", suma);
